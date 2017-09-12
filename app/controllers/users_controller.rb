2a846f4c9a2b06f0a32f6c03db0ee1c6f606f1b4
Teja <tejalevaku@gmail.com>
2017-09-12 12:59:19 +0530

    created controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!
+
+ def index
+ 	@users = User.all
+ end
+ def new
+  @user = User.new	
+ end
+ def create
+ @user = User.new(user_params)
+ if @user.save
+ redirect_to users_path, notice: "successfully added user"
+ else
+ render action: "new"
+ end
+ end
+
+ def show 
+ 	@user = User.new(user_params)
+ end
+ private
+
+ def user_params
+ 	params[:user].permit(:email, :password,:first_name, :last_name, :username, :mobile,  role_ids:[])
+ end	
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
+
+      before_create :assign_role
+
+
+      private
+      def assign_role
+      	self.role = role.last
+       end
 end
