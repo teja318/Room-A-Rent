661eb7ef27a4625cfa5aac15dada5ba7f8a7a46b
teja318 <teja@gmail.com>
2017-09-20 13:38:11 +0530

    generated controller special_prices

---
+++ app/helpers/special_price_helper.rb
@@ -0,0 +1,2 @@
+module SpecialPriceHelper
+end
---
+++ test/controllers/special_price_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SpecialPriceControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
