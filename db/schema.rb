004359133ae19fe6145bd879c324d6074128e39d
teja318 <teja@gmail.com>
2017-09-20 13:49:06 +0530

    generated model reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920075345) do
+ActiveRecord::Schema.define(version: 20170920081233) do
 
   create_table "amenities", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,19 @@ ActiveRecord::Schema.define(version: 20170920075345) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "review"
+    t.integer  "food_rating"
+    t.integer  "cleanliness_rating"
+    t.integer  "safety_rating"
+    t.integer  "facility_rating"
+    t.integer  "locality_rating"
+    t.integer  "room_id"
+    t.integer  "user_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "roles", force: :cascade do |t|
